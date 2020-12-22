3367541f4ea03d9c78a8047646566f23cc264353
timkang93 <jeongmo.kang@gmail.com>
2020-12-21 16:24:57 -0800

    [feat] call splash art from S3

--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Splash = () => {
+  const StyledSplashHeaderSummonerName = styled.h2`
+    font-family: 'Roboto', san-serif;
+  `;
+  return (
+    <div id="splash">
+      <img src="https://nglol.s3-us-west-1.amazonaws.com/champion/splash/Aatrox_0.jpg" />
+      <StyledSplashHeaderSummonerName>Jeongmo</StyledSplashHeaderSummonerName>
+    </div>
+  );
+}
+
+export default Splash;
