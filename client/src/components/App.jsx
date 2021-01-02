a5f0fdc01ae30b2ce773b73ca83e991605ebb46c
timkang93 <jeongmo.kang@gmail.com>
2021-01-02 02:05:50 -0800

    [refactor] move data shaping from frontend to backend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,61 +15,29 @@ const App = () => {
 
   function getSummonerInfo(summonerName) {
     axios.get(`/summoner/${summonerName}`)
-      .then((results) => {
+      .then(results => {
         setSummonerName(results.data.name);
-        return Promise.all([
-          axios.get(`/gameModes/${results.data.id}`),
-          results.data.accountId
-        ]);
+        return axios.get(`/gameModes/${results.data.id}&${results.data.accountId}`)
       })
-      .then(([queueType, accountId]) => {
-        let totalRankGames = 0;
-        const queueTypeArr = queueType.data;
-
-        for (let i = 0; i < queueTypeArr.length; i++) {
-          if (queueTypeArr[i].queueType === "RANKED_SOLO_5x5") {
-            totalRankGames = queueTypeArr[i].wins + queueTypeArr[i].losses;
-          }
-        }
+      .then(results => {
+        const totalRankGames = results.data.totalRankGames;
+        const accountId = results.data.accountId;
+        const start = 0;
+        const end = 10;
 
         if (!totalRankGames) {
           throw new Error(`You don't have any ranked solo games played`)
         }
 
-        function axiosGetMatches(start, end) {
-          return axios.get(`/matchHistoryPage/${accountId}&${start}&${end}`);
-        }
-
-        let matchHistoryPage = [];
-        matchHistoryPage.push(axiosGetMatches(0, Math.min(10, totalRankGames)));
-        // let length = Math.floor(totalRankGames / 100);
-        // for (let j = 0; j <= length; j++) {
-        //   matchHistoryPage.push(axiosGetMatches(j * 100, Math.min(j * 100 + 100, totalRankGames)));
-        // }
-
-        return Promise.all(matchHistoryPage);
+        return axios.get(`/matchHistoryPage/${accountId}&${start}&${end}`);
       })
       .then((results) => {
-        let matchStorage = [];
-        let playedChampions = {};
-        for (let i = 0; i < results.length; i++) {
-          let matchHistory = results[i].data.matches;
-          for (let j = 0; j < matchHistory.length; j++) {
-            // count how many times each champion was played
-            let champion = matchHistory[j].champion;
-            if (!playedChampions[champion]) {
-              playedChampions[champion] = 1;
-            } else {
-              playedChampions[champion]++;
-            }
-            matchStorage.push(axios.get(`/individualMatch/${matchHistory[j].gameId}`));
-          }
-        }
-
-        let mostChampionId = Object.keys(playedChampions).reduce((a, b) => playedChampions[a] > playedChampions[b] ? a : b);
-        let mostChampionName = getChampionName(mostChampionId);
+        let mostChampionName = getChampionName(results.data.mostChampionId);
         setMostChampionName(mostChampionName);
-        return Promise.all(matchStorage)
+
+        const matchStorage = results.data.gameIds.map(gameId => axios.get(`/individualMatch/${gameId}`));
+
+        return Promise.all(matchStorage);
       })
       .then((results) => {
         let datas = results.map(({data}) => data);
@@ -82,7 +50,7 @@ const App = () => {
 
   return (
     <div>
-      {mostChampionName != '' && <Splash mostChampionName={mostChampionName}/>}
+      {mostChampionName != '' && <Splash mostChampionName={mostChampionName} />}
       <div>Summoner Name: {summonerName}</div>
       <div>Most Played Champion: {mostChampionName}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/summoner/:id', (req, res) => {
-  axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=${api_key}`)
+app.get('/summoner/:summonerName', (req, res) => {
+  axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.summonerName}?api_key=${api_key}`)
     .then((results) => {
       res.status(200).send(results.data);
     })
@@ -28,10 +28,21 @@ app.get('/summoner/:id', (req, res) => {
     });
 });
 
-app.get('/gameModes/:id', (req, res) => {
+app.get('/gameModes/:id&:accountId', (req, res) => {
   axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${req.params.id}?api_key=${api_key}`)
     .then(results => {
-      res.status(200).send(results.data);
+      let totalRankGames = 0;
+      let accountId = req.params.accountId;
+      const queueTypeArr = results.data;
+
+      for (let i = 0; i < queueTypeArr.length; i++) {
+        if (queueTypeArr[i].queueType === "RANKED_SOLO_5x5") {
+          totalRankGames = queueTypeArr[i].wins + queueTypeArr[i].losses;
+        }
+      }
+
+      const data = { totalRankGames, accountId };
+      res.status(200).send(data);
     })
     .catch(err => {
       res.status(400).send(err);
@@ -41,7 +52,24 @@ app.get('/gameModes/:id', (req, res) => {
 app.get('/matchHistoryPage/:accountId&:start&:end', (req, res) => {
   axios.get(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${req.params.accountId}?queue=420&endIndex=${req.params.end}&beginIndex=${req.params.start}&api_key=${api_key}`)
     .then(results => {
-      res.status(200).send(results.data);
+      let gameIds = [];
+      let champion = 0;
+      const playedChampions = {};
+      let matchHistory = results.data.matches;
+
+      for (let j = 0; j < matchHistory.length; j++) {
+        gameIds.push(matchHistory[j].gameId);
+        champion = matchHistory[j].champion;
+
+        if (!playedChampions[champion]) {
+          playedChampions[champion] = 1;
+        } else { playedChampions[champion]++; }
+      }
+
+      let mostChampionId = Object.keys(playedChampions).reduce((a, b) => playedChampions[a] > playedChampions[b] ? a : b);
+
+      const data = { mostChampionId, gameIds };
+      res.status(200).send(data);
     })
     .catch(err => {
       res.status(400).send(err);
