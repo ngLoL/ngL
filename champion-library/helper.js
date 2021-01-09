e05d72c6473ead48dc41565ce9cc5b97b6684a12
timkang93 <jeongmo.kang@gmail.com>
2021-01-08 18:22:09 -0800

    [prog] generate champId-champName key-value JSON to reduce bundle.js size

--- champion-library/helper.js
+++ champion-library/helper.js
@@ -1,13 +1,7 @@
-var championJson = require('./champion.json');
-
-let championsData = JSON.parse(JSON.stringify(championJson)).data;
+var championJson = require('./champIdToName.json');
 
 module.exports = {
   getChampionName: (championId) => {
-    for (var champion in championsData) {
-      if (championsData[champion].key == championId) {
-        return champion;
-      }
-    }
+    return championJson[championId];
   }
 }
