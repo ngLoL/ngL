f8c6610a2c8146c30d2441ab5a3945c308340773
Jason Hong <jasonhong1240@gmail.com>
2021-01-26 22:28:21 -0800

    update visionScore.jsx with confetti

--- client/src/components/visionScore.jsx
+++ client/src/components/visionScore.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {IoMdTrophy} from 'react-icons/io';
+import Confetti from 'react-dom-confetti';
 import Caption from './reuse/Caption.js';
 import ContentWrapper from './reuse/ContentWrapper.js';
 import SectionWrapper from './reuse/SectionWrapper.js';
+
+const leftConfetti = {
+  angle: 45,
+  spread: 30,
+};
+
+const rightConfetti = {
+  angle: 135,
+  spread: 30,
+}
 
 const StyledTrophy = styled(IoMdTrophy)`
   color: #ecd610;
@@ -12,7 +23,7 @@ const StyledTrophy = styled(IoMdTrophy)`
 
 const StyledVisionScore = styled.div`
   color: #000;
-  font-size: 5rem;
+  font-size: 4rem;
   position: absolute;
   margin-top: -10rem;
 `;
@@ -25,16 +36,42 @@ const TrophyContainer = styled.div`
   align-items: center;
 `;
 
+const ConfettiContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const TrophyName = styled.div`
+  color: #000;
+  font-size: 1.5rem;
+  position: absolute;
+  margin-top: 21.6rem;
+`;
+
+
 const VisionScore = ({children, visionScore, numGames}) => {
+  const [confetti, setConfetti] = useState(false);
+
+  const visibility = (boolean) => {
+    if (boolean) {
+      return <StyledVisionScore>{Math.round(100*(visionScore / numGames)) / 100}</StyledVisionScore>;
+    }
+  }
   return (
     <SectionWrapper>
       <ContentWrapper>
         <Caption>{children}</Caption>
-        <Caption>AVG VISION SCORE</Caption>
-        <TrophyContainer>
+        <Caption>Click on the Trophy!</Caption>
+        <TrophyContainer onClick={() => setConfetti(true)}>
           <StyledTrophy></StyledTrophy>
-          <StyledVisionScore>{Math.round(100*(visionScore / numGames)) / 100}</StyledVisionScore>
+          {visibility(confetti)}
+          <TrophyName>AVG VISION SCORE</TrophyName>
         </TrophyContainer>
+        <ConfettiContainer>
+          <Confetti active={confetti} config={leftConfetti}></Confetti>
+          <Confetti active={confetti} config={rightConfetti}></Confetti>
+        </ConfettiContainer>
       </ContentWrapper>
     </SectionWrapper>
   )
